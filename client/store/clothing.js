0b9a9818e7114fd88af5ccfe384e9af6620b4cee
Nicole Ivanov <nivanov1030@gmail.com>
2017-09-22 16:58:10 -0400

    add clothing reducer in store

---
+++ client/store/clothing.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_CLOTHING = 'GET_CLOTHING'
+
+/**
+ * INITIAL STATE
+ */
+const defaultClothing = []
+
+/**
+ * ACTION CREATORS
+ */
+const getClothing = clothing => ({type: GET_CLOTHING, clothing})
+
+
+/**
+ * REDUCER
+ */
+export default function (state = defaultClothing, action) {
+  switch (action.type) {
+    case GET_CLOTHING:
+      return action.clothing
+    default:
+      return state
+  }
+}
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchAllClothing = () => {
+  return async dispatch => {
+    try {
+      const clothingObjs = await axios.get('/api/clothing')
+      const clothing = clothingObjs.data
+      dispatch(getClothing(clothing))
+    } catch(e) {
+      console.error(e)
+    }
+  }
+}
+
